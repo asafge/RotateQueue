1392de8bf6ebb515ac6afb6a101605f87c02359c
Asaf Geva <asaf@purplebit.com>
2014-03-12 11:47:12 +0200

    Fix indentation

--- RotateQueue.java
+++ RotateQueue.java
@@ -9,7 +9,7 @@ public class  RotateQueue<E> {
 	public List<E> Elements;
 	public int Capacity;
 
-    // Constructor, creates a RotateQueue from a comma delimited String.
+	// Constructor, creates a RotateQueue from a comma delimited String.
 	public RotateQueue(int capacity, String serialized) {
 		Elements = new ArrayList<E>(capacity);
 		Capacity = capacity;
@@ -21,22 +21,22 @@ public class  RotateQueue<E> {
 		}
 	}
 
-    // Add a new element to the queue, clean-up old elements if needed
+	// Add a new element to the queue, clean-up old elements if needed
 	public synchronized void AddElement(E value) {
 		if (Capacity == Elements.size()) {
-            int deleteFrom = (int)Math.ceil(Capacity * 0.05);
-            Elements = Elements.subList(deleteFrom, Capacity);
-        }
+			int deleteFrom = (int)Math.ceil(Capacity * 0.05);
+			Elements = Elements.subList(deleteFrom, Capacity);
+		}
 		if (!this.SearchElement(value)) {
 			Elements.add(value);
-        }
+		}
 	}
 
-    // Search an element by value
+	// Search an element by value
 	public synchronized boolean SearchElement(E value) {
 		return Elements.contains(value);
 	}
-	
+
 	@Override
 	public synchronized String toString() {
 		StringBuilder sb = new StringBuilder();
