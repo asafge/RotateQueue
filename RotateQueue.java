4a811e3f22b9b4ec279e91fe4fe9c02a0c653a2c
Asaf Geva <asaf@purplebit.com>
2014-03-12 12:12:58 +0200

    Added another constructor

--- RotateQueue.java
+++ RotateQueue.java
@@ -9,17 +9,30 @@ public class  RotateQueue<E> {
 	public List<E> Elements;
 	public int Capacity;
 
-	// Constructor, creates a RotateQueue from a comma delimited String.
+	// Constructor, creates a RotateQueue of Strings from a comma delimited String.
 	public RotateQueue(int capacity, String serialized) {
 		Elements = new ArrayList<E>(capacity);
 		Capacity = capacity;
 		if (serialized.trim().length() != 0) {
 			String[] values = serialized.split(",", capacity);
-			for (String v : values)
-				if (v.trim().length() != 0)
-					this.AddElement((E)v);
+			for (String v : values) {
+				if (v.trim().length() != 0) {
+                    @SuppressWarnings("unchecked")
+                    E item = (E)v;
+					this.AddElement(item);
+                }
+            }
 		}
 	}
+
+    // Constructor, creates a RotateQueue from an ArrayList<E>
+    public RotateQueue(int capacity, ArrayList<E> items) {
+        Elements = new ArrayList<E>(capacity);
+        Capacity = capacity;
+        for (E item : items) {
+            this.AddElement(item);
+        }
+    }
 
 	// Add a new element to the queue, clean-up old elements if needed
 	public synchronized void AddElement(E value) {
